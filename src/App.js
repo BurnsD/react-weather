34e3322001ffab14f8b6d32eba6a600981a2a3ee
BurnsD <Dustinburnsactech@gmail.com>
2022-05-13 12:04:43 -0500

    hard code for app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  // const url ='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=dc85d6952867a5783f19399f13c6e547'
+
+
   return (
     <div className="App">
+      <div className="container">
 
+        <div className="top">
+          <div className="location"> 
+           <p>Dallas</p>
+          </div>
+          <div className="tempature">
+            <h1> 91F </h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels-like">
+            <p>81</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>21MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
